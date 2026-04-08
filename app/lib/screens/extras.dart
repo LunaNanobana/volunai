8df4214d3bddb987a723cdd83d46b9970bd94a93
Aiym Kaspakova <144852064+LunaNanobana@users.noreply.github.com>
2026-04-08 20:47:15 +0500

    Delete app/lib/screens directory

--- app/lib/screens/extras.dart
+++
@@ -1,4 +0,0 @@
-export { ShelterQRScreen } from './remaining_screens';
-export { SafetyPlanScreen } from './remaining_screens';
-export { PrivacyScreen } from './remaining_screens';
-export { PanicScreen } from './remaining_screens';
